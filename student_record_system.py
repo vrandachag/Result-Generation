ce5ce0c528abd4df60c00dec67bd55528f103420
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2021-05-07 17:45:57 +0530

    Create student_record_system.py

---
+++ student_record_system.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    database = "student_record_system",
+    user = "root",
+    password = "Helloworld123"
+#    password = input("Enter password:"),
+    )
+
+mycursor = mydb.cursor()
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
+
+sql = "INSERT INTO student_marks(Scholar_Number,Subject_Code,Marks_obtained,Max_Marks,Class,Term,Year) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+val = (
+    input("Enter scholar number:"),
+    input("Enter Subject code:"),
+    input("Enter Marks Obtained:"),
+    input("Enter Max Marks:"),
+    input("Enter Class:"),
+    input("Enter term:"),
+    input("Enter year:")
+    )
+mycursor.execute(sql,val)
+mydb.commit()
+
+mydb.close()
